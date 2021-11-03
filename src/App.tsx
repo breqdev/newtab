24c38804b106e4fc7ba1a9e4ca928bf16e044c81
Brooke Chalmers <breq@breq.dev>
2021-11-03 15:15:42 -0400

    Initial commit + clock

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Clock from "./Clock";
+
+function App() {
+  return (
+    <div className="w-screen h-screen grid place-items-center">
+      <div className="flex">
+        <Clock />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Clock.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+function useTime() {
+  const [time, setTime] = React.useState(new Date());
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return time;
+}
+
+interface TimezoneClockProps {
+  timezone: string;
+  friendlyName: string;
+}
+
+function TimezoneClock({ timezone, friendlyName }: TimezoneClockProps) {
+  const time = useTime();
+
+  const timeInZone = new Date(
+    time.toLocaleString("en-US", {
+      timeZone: timezone,
+    })
+  );
+
+  const offset =
+    Math.round((timeInZone.getTime() - time.getTime()) / 3600000) -
+    Math.round(time.getTimezoneOffset() / 60);
+
+  const offsetPadding = offset == 0 ? "\xA0" : offset > 0 ? "+" : "";
+
+  return (
+    <tr className="font-mono">
+      <td>{friendlyName}</td>
+      <td>{offsetPadding + offset}</td>
+      <td>
+        {timeInZone.toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: false,
+        })}
+      </td>
+    </tr>
+  );
+}
+
+export default function Clock() {
+  const time = useTime();
+
+  const zones: TimezoneClockProps[] = [
+    {
+      timezone: "UTC",
+      friendlyName: "UTC",
+    },
+    {
+      timezone: "America/New_York",
+      friendlyName: "NYC",
+    },
+    {
+      timezone: "America/Los_Angeles",
+      friendlyName: "LAX",
+    },
+    {
+      timezone: "Europe/Berlin",
+      friendlyName: "BER",
+    },
+    {
+      timezone: "Europe/London",
+      friendlyName: "LHR",
+    },
+    {
+      timezone: "Asia/Shanghai",
+      friendlyName: "PEK",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col text-center border-2 border-black rounded-xl p-8">
+      <span className="text-7xl mb-1">
+        {time.toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: false,
+        })}
+      </span>
+      <span className="text-3xl">{time.toLocaleDateString()}</span>
+      <hr className="border-black my-8" />
+      <table>
+        <tr>
+          <th>zone</th>
+          <th>offset</th>
+          <th>time</th>
+        </tr>
+        {zones.map((zone) => (
+          <TimezoneClock key={zone.timezone} {...zone} />
+        ))}
+      </table>
+    </div>
+  );
+}
